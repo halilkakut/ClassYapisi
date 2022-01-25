d6b508845c28fc12eb80105c8ea9ea50b9167eaf
Halil İbrahim Kakut <halil.kakut@hotmail.com>
2022-01-25 13:02:46 +0300

    Add Constructor

--- ExampleClass-1/Program.cs
+++ ExampleClass-1/Program.cs
@@ -7,20 +7,27 @@ namespace ExampleClass_1
         static void Main(string[] args)
         {
             //Söz Dizimi
-            Calisan calisan1 = new Calisan();
-            calisan1.Ad = "Halil";
-            calisan1.Soyad = "Kakut";
-            calisan1.No = 23243252;
-            calisan1.Departman = "İnsan Kaynakları";
-
+            Console.WriteLine("******** Çalışan 1 **********");
+            Calisan calisan1 = new Calisan("Halil","Kakut",2324566,"İnsan Kaynakları");
             calisan1.CalisanBilgileri();
+            // calisan1.Ad = "Halil";
+            // calisan1.Soyad = "Kakut";
+            // calisan1.No = 23243252;
+            //calisan1.Departman = "İnsan Kaynakları";
 
+            
+            
+            Console.WriteLine("********* Çalışan 2 Bilgileri **********");
             Calisan calisan2 = new Calisan();
             calisan2.Ad = "Ahmet";
             calisan2.Soyad = "yılmaz";
             calisan2.No = 43536774;
             calisan2.Departman = "Satın Alma";
             calisan2.CalisanBilgileri();
+
+            Console.WriteLine("******** Çalışan 3 Bilgileri **********");
+            Calisan calisan3 = new Calisan("Şeref","Kakut");
+            calisan3.CalisanBilgileri();
         }
     }
 
@@ -31,6 +38,23 @@ namespace ExampleClass_1
         public int No;
         public string Departman;
 
+        public Calisan(string ad, string soyad, int no, string departman)
+        {
+            this.Ad = ad;
+            this.Soyad = soyad;
+            this.No = no;
+            this.Departman = departman;
+        }
+        public Calisan(string ad, string soyad)
+        {
+            this.Ad = ad;
+            this.Soyad = soyad;
+        }
+
+        public Calisan(){
+
+        }
+
         public void  CalisanBilgileri()
         {
             Console.WriteLine("Çalışma Adı:{0}",Ad);
@@ -38,5 +62,7 @@ namespace ExampleClass_1
             Console.WriteLine("Çalışanın Numarası:{0}",No);
             Console.WriteLine("Çalışanın Departmanı:{0}",Departman);
         }
+
+
     }
 }
